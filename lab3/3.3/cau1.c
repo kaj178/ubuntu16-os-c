2e2f96692c6a4c3f8147af4d5e5c5b069ac79bbc
Anh Khoa <anhkhoa5525@gmail.com>
2023-07-20 16:30:57 +0700

    Update 3.3

--- lab3/3.3/cau1.c
+++ lab3/3.3/cau1.c
@@ -8,3 +8,31 @@ void inPid(pid_t p) {
     printf("ID cua process con la %d\n", p);
 }
 
+int main(int argc, char ** const argv) {
+    printf("Current process: %d\n", getpid());
+    pid_t pidBC;
+    printf("Tien trinh A\n");
+    inPid(pidBC);
+    pidBC = fork();
+    if (pidBC == 0) {
+        pid_t pidDE = fork();
+        if (pidDE > 0) {
+            printf("Tien trinh E\n");
+            inPid(pidDE);
+            wait(NULL);
+        }
+    }
+    else if (pidBC > 0) {
+        printf("Tien trinh C\n");
+        inPid(pidBC);
+        pid_t pidH;\
+        pidH = fork();
+        if (pidH > 0) {
+            wait(NULL);
+            printf("Tien trinh H\n");
+            inPid(pidH);
+        }
+        wait(NULL);
+    }
+    return 0;
+}
